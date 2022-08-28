9187db4011b2941d9ed05629c1501c8d9f55ae8e
Danilo de Maria <daniloctg@msn.com>
2022-08-28 18:14:27 -0300

    validation on input user, with cpf and cnpj

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,25 @@ const express = require('express');
 const router = express.Router();
 const Firebird = require('node-firebird');
 const { getOS } = require('../querys/getOs');
+const { cpf, cnpj } = require('cpf-cnpj-validator');
 
-router.get('/', async (req, res) => {
+const validateAndFormatDocument = (document) => {
+  const rawDocument = document.replace(/[^\d]/g, '');
+  const type = rawDocument.length === 11 ? 'CPF' : 'CNPJ';
+  const valid =
+    type === 'CPF' ? cpf.isValid(rawDocument) : cnpj.isValid(rawDocument);
+  if (!valid) throw ApiError.badRequest('Documento inválido');
+  return {
+    document_type: type,
+    rawDocument,
+  };
+};
+
+router.get('/:document', async (req, res) => {
+  const {
+    params: { document },
+  } = req;
+
   const options = {
     host: process.env.DATABASE_URL,
     port: process.env.DATABASE_PORT,
@@ -14,16 +31,24 @@ router.get('/', async (req, res) => {
     role: process.env.DATABASE_ROLE,
     pageSize: process.env.DATABASE_PAGE_SIZE,
   };
+  const { document_type, rawDocument } = validateAndFormatDocument(document);
+  const columnDatabase = document_type === 'CPF' ? CPF : 'CNPJ';
+  const query = `SELECT IDCLIFOREMP,TIPOCLIFOREMP, FANTASIA, RAZAO, IDCODIGOAGRUPA FROM CLIFOREMP WHERE ${columnDatabase}='${rawDocument}' `;
+
   Firebird.attach(options, function (err, db) {
     if (err) {
       console.log(err);
       return res.status(500).send({ message: 'Erro na consulta', err });
     }
 
-    db.query('SELECT * FROM ACESSO_API', function (err, result) {
+    db.query(query, function (err, result) {
       // IMPORTANT: close the connection
+      const ids = result.map(({ idcliforemp, idcodigoagrupa }) => ({
+        idcliforemp,
+        idcodigoagrupa,
+      }));
       db.detach();
-      return res.status(200).send(result);
+      return res.status(200).send({ ids, result });
     });
   });
 });
